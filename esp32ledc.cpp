b985c3218084a7ebace2802b53773607b8781c77
Emil Muratov <gpm@hotplug.ru>
2022-01-23 01:40:59 +0300

    Created PWMController singleton wrapper for ESP32 LEDC pwm

---
+++ esp32ledc.cpp
@@ -0,0 +1,267 @@
+/*
+    ESP32 Light Manager library
+
+This code implements a library for ESP32-xx family chips and provides an
+API for controling Lighting applications, mostly (but not limited to) LEDs,
+LED strips, PWM drivers, RGB LED strips etc...
+
+
+Copyright (C) Emil Muratov, 2022
+GitHub: https://github.com/vortigont/ESP32-LightManager
+
+ *  This program or library is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General
+ *  Public License along with this library; if not, get one at
+ *  https://opensource.org/licenses/LGPL-3.0
+*/
+
+#include "esp32ledc.hpp"
+
+
+// static member must be defined outside the class scope
+EventGroupHandle_t PWMCtl::g_fade_evt = nullptr;
+
+
+PWMCtl::PWMCtl(){
+  tmInit();
+  chInit();
+  // fade_func required for thread safe ledc_set_duty_and_update() function to work
+  ledc_fade_func_install(0);
+}
+
+PWMCtl::~PWMCtl(){
+  ledc_fade_func_uninstall();
+}
+    //t_cfg.speed_mode = (ledc_mode_t)(i/LEDC_TIMER_MAX);
+    //t_cfg.timer_num = (ledc_timer_t)(i%LEDC_TIMER_MAX);
+
+// construct channel default cfg
+void PWMCtl::chInit(){
+  ledc_channel_config_t c_cfg = {
+    -1,                       // int gpio_num;                   /*!< the LEDC output gpio_num, if you want to use gpio16, gpio_num = 16 */
+    LEDC_LOW_SPEED_MODE,      // ledc_mode_t speed_mode;         /*!< LEDC speed speed_mode, high-speed mode or low-speed mode */
+    LEDC_CHANNEL_0,           // ledc_channel_t channel;         /*!< LEDC channel (0 - 7) */
+    LEDC_INTR_DISABLE,        // ledc_intr_type_t intr_type;     /*!< configure interrupt, Fade interrupt enable  or Fade interrupt disable */
+    LEDC_TIMER_0,             // ledc_timer_t timer_sel;         /*!< Select the timer source of channel (0 - 3) */
+    DEFAULT_PWM_DUTY,         // uint32_t duty;                  /*!< LEDC channel duty, the range of duty setting is [0, (2**duty_resolution)-1] */
+    0,                        // int hpoint;                     /*!< LEDC channel hpoint value, the max value is 0xfffff */
+    {0}                       // unsigned int output_invert: 1;/*!< Enable (1) or disable (0) gpio output invert */
+  };
+
+  for (unsigned i = 0; i < LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX; ++i){
+    channels[i].cfg = c_cfg;
+    channels[i].cfg.speed_mode = (ledc_mode_t)(i/LEDC_CHANNEL_MAX);
+    channels[i].cfg.channel = (ledc_channel_t)(i%LEDC_CHANNEL_MAX);
+  }
+}
+
+// set channel duty
+esp_err_t PWMCtl::chDuty(uint32_t ch, uint32_t duty){
+  return chDutyPhase(ch, duty, channels[ch % (LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX)].cfg.hpoint);
+}
+
+esp_err_t PWMCtl::chPhase(uint32_t ch, uint32_t phase){
+  return chDutyPhase(ch, channels[ch % (LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX)].cfg.duty, phase);
+}
+
+esp_err_t PWMCtl::chDutyPhase(uint32_t ch, uint32_t duty, uint32_t phase){
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+
+  channels[ch].cfg.duty = duty;
+  channels[ch].cfg.hpoint = phase;
+
+/*
+  if (!channels[ch].initialized){
+    printf("err set duty for ch:%d\n", ch);
+    return ESP_ERR_INVALID_STATE;
+  }
+*/
+
+  printf("Set Channel:%d, duty:%d, phase:%d\n", ch, duty, phase);
+  return ledc_set_duty_and_update(channels[ch].cfg.speed_mode, channels[ch].cfg.channel, duty, phase);
+  //ledc_set_duty(channels[ch].cfg.speed_mode, channels[ch].cfg.channel, duty);
+  //ledc_update_duty(channels[ch].cfg.speed_mode, channels[ch].cfg.channel);
+};
+
+uint32_t PWMCtl::chGetDuty(uint32_t ch) const {
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+  return ledc_get_duty(channels[ch].cfg.speed_mode, channels[ch].cfg.channel);
+}
+
+int PWMCtl::chStart(uint32_t ch, int pin){
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+
+  if (pin > 0)
+    channels[ch].cfg.gpio_num = pin;
+
+  // check if we are already running
+  if (channels[ch].state == ch_state::active)
+    return ESP_OK;
+
+  // configure channel
+  if (chCfg(ch))
+    return ESP_ERR_INVALID_STATE;
+
+  if (chFadeISR(ch, channels[ch].fade_cb))
+    return ESP_ERR_INVALID_STATE;
+
+  // run attached Timer (if needed)
+  uint8_t t = channels[ch].cfg.timer_sel + ch/LEDC_CHANNEL_MAX * LEDC_TIMER_MAX;
+  if (tmStart(t)){
+    return ESP_ERR_INVALID_STATE;
+  }
+
+  channels[ch].state = ch_state::active;
+  printf("channel:%d started as LEDC ch:%d, mode:%d\n", ch, channels[ch].cfg.channel, channels[ch].cfg.speed_mode);
+  return ESP_OK;
+}
+
+int PWMCtl::chStop(uint32_t ch){
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+  return ledc_stop(channels[ch].cfg.speed_mode, channels[ch].cfg.channel, channels[ch].idle_level);
+}
+
+int PWMCtl::chAttachTimer(uint32_t ch, uint8_t timer){
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+  timer %= LEDC_TIMER_MAX;
+
+  channels[ch].cfg.timer_sel = (ledc_timer_t)(timer);
+  return ledc_bind_channel_timer(channels[ch].cfg.speed_mode, channels[ch].cfg.channel, channels[ch].cfg.timer_sel);
+}
+
+int PWMCtl::chSet(uint32_t ch, int pin, bool idlelvl, bool invert){
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+
+  printf("Configuring pin %d for ch:%d / ledcch:%d\n", pin, ch, channels[ch].cfg.channel);
+
+  if (channels[ch].cfg.channel >= LEDC_CHANNEL_MAX){
+    printf("Cfg corrupted for ch:%d / ledcch:%d\n", ch, channels[ch].cfg.channel);
+    return ESP_ERR_INVALID_STATE;
+  }
+
+  channels[ch].cfg.gpio_num = pin;
+  channels[ch].cfg.flags.output_invert = invert;
+  channels[ch].idle_level = idlelvl;
+
+  ledc_stop(channels[ch].cfg.speed_mode, channels[ch].cfg.channel, channels[ch].idle_level);
+  return chCfg(ch);
+}
+
+int PWMCtl::chCfg(uint32_t ch){
+  if (channels[ch].cfg.gpio_num == -1){
+    printf("err pin is not set for ch:%d\n", ch);
+    return ESP_ERR_INVALID_STATE;
+  }  // pin is not set
+
+  if (ledc_channel_config(&channels[ch].cfg)){
+    printf("err cfg ch:%d\n", ch);
+    channels[ch].state = ch_state::stop;
+    return ESP_ERR_INVALID_STATE;
+  }
+
+  printf("cfg set for ch:%d\n", ch);
+  return ESP_OK;
+}
+
+int PWMCtl::chFadeISR(uint32_t ch, bool enable){
+  ch %= LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX;
+  channels[ch].fade_cb = enable;
+
+  if (channels[ch].fade_cb){
+    ledc_cbs_t cbs = { .fade_cb = isr_fade };
+    return ledc_cb_register(channels[ch].cfg.speed_mode, channels[ch].cfg.channel, &cbs, nullptr); // (void *)ch
+  }
+  return ESP_OK;
+  // TODO: is there any way to detach channel fade ISR cb? /it, is!/
+}
+
+
+void PWMCtl::tmInit(){
+  ledc_timer_config_t t_cfg = {
+    LEDC_LOW_SPEED_MODE,      // .speed_mode
+    DEFAULT_PWM_RESOLUTION,   // .duty_resolution / resolution of PWM duty
+    LEDC_TIMER_0,             // .timer_num / timer index
+    DEFAULT_PWM_FREQ,         // .freq_hz / frequency of PWM signal
+    DEFAULT_PWM_CLK           // .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
+  };
+
+  for (unsigned i = 0; i < LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX; ++i){
+    timers[i].cfg = t_cfg;
+    timers[i].cfg.speed_mode = (ledc_mode_t)(i/LEDC_TIMER_MAX);
+    timers[i].cfg.timer_num = (ledc_timer_t)(i%LEDC_TIMER_MAX);
+  }
+}
+
+int PWMCtl::tmStart(uint8_t tm){
+    tm %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX;
+    if ((uint8_t)timers[tm].state > 0)
+      return ESP_OK;
+
+    if(ledc_timer_config(&timers[tm].cfg)){
+      printf("err cfg timer:%d\n", tm);
+      timers[tm].state = tm_state::stop;
+      return ESP_ERR_INVALID_STATE;
+    }
+
+    printf("Configured timer %d\n", tm);
+    timers[tm].state = tm_state::active;
+    return ESP_OK;
+}
+
+esp_err_t PWMCtl::tmSet(uint8_t tm, ledc_timer_bit_t bits, uint32_t hz){
+  tm %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX;
+  timers[tm].cfg.duty_resolution = bits;
+  timers[tm].cfg.freq_hz = hz;
+  return ledc_timer_config(&timers[tm].cfg);
+}
+
+esp_err_t PWMCtl::tmSetFreq(uint8_t tm, uint32_t hz){
+  tm %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX;
+  timers[tm].cfg.freq_hz = hz;
+  return ledc_set_freq(timers[tm].cfg.speed_mode, timers[tm].cfg.timer_num, hz);
+}
+
+uint32_t PWMCtl::tmGetFreq(uint8_t tm) const {
+  tm %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX;
+  return ledc_get_freq(timers[tm].cfg.speed_mode, timers[tm].cfg.timer_num);
+}
+
+EventGroupHandle_t* PWMCtl::getFaderEventGroup(){
+  // create MsgGroup
+  if (!g_fade_evt)
+    g_fade_evt = xEventGroupCreate();
+
+  return &g_fade_evt;
+}
+
+bool IRAM_ATTR PWMCtl::isr_fade(const ledc_cb_param_t *param, void *arg){
+        portBASE_TYPE taskAwoken = pdFALSE;
+
+        if (g_fade_evt && (param->event == LEDC_FADE_END_EVT)) {
+            uint32_t ch = param->channel * (param->speed_mode ? 2 : 1);
+            xEventGroupSetBitsFromISR(g_fade_evt, (1<<ch), &taskAwoken);
+            //xEventGroupSetBitsFromISR(g_fade_evt, (1<<(uint32_t)arg), &taskAwoken);
+        }
+
+        return (taskAwoken == pdTRUE);  // a context switch may take place in a calee isr
+    }
+
+
+uint32_t PWMCtl::chGetMaxDuty(uint32_t ch) const {
+    ch %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX;   // return wrap_ledc_get_max_duty(channels[ch].cfg.speed_mode, channels[ch].cfg.channel); 
+
+    uint8_t chtimer = channels[ch].cfg.timer_sel;
+    if (ch / LEDC_CHANNEL_MAX)      // check if it's a LS esp32 channel
+      chtimer += LEDC_TIMER_MAX;
+
+    return (1 << timers[chtimer].cfg.duty_resolution) - 1;
+};
---
+++ esp32ledc.hpp
@@ -0,0 +1,213 @@
+/*
+    ESP32 Light Manager library
+
+This code implements a library for ESP32-xx family chips and provides an
+API for controling Lighting applications, mostly (but not limited to) LEDs,
+LED strips, PWM drivers, RGB LED strips etc...
+
+
+Copyright (C) Emil Muratov, 2022
+GitHub: https://github.com/vortigont/ESP32-LightManager
+
+ *  This program or library is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General
+ *  Public License along with this library; if not, get one at
+ *  https://opensource.org/licenses/LGPL-3.0
+*/
+
+#pragma once
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "driver/ledc.h"
+
+#ifdef ARDUINO
+#include "esp32-hal-log.h"
+#else
+#include "esp_log.h"
+#endif
+
+#define DEFAULT_PWM_FREQ            2000
+#define DEFAULT_PWM_RESOLUTION      LEDC_TIMER_10_BIT
+#define DEFAULT_PWM_CLK             LEDC_AUTO_CLK
+#define DEFAULT_PWM_DUTY            (1<<DEFAULT_PWM_RESOLUTION - 1)     // 50% duty
+#define DEFAULT_MAX_DUTY            ((1<<DEFAULT_PWM_RESOLUTION) - 1)
+
+#if configUSE_16_BIT_TICKS
+#define MAX_EG_BITS                 8
+#else
+#define MAX_EG_BITS                 24
+#endif
+
+#define CH_EVENTS_BIT_MASK          ((1 << LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX) - 1)
+
+// macro's
+#define BIT_SET(var, bit) (var |= 1<<bit)
+#define BIT_CLR(var, bit) (var &= ~(1<<bit))
+#define BIT_READ(var, bit) ((var >> bit) & 1)
+
+// ESP32 log tag
+static const char *TAG __attribute__((unused)) = "LEDC";
+
+/*
+refs:
+    Table 69: Commonly­ used Frequencies and Resolutions
+    RTC8M_CLK (8 MHz) 8 kHz 1/512 (9 bit)
+*/
+
+// need this until https://github.com/espressif/esp-idf/pull/8247
+uint32_t wrap_ledc_get_max_duty(ledc_mode_t speed_mode, ledc_channel_t channel);
+
+enum class ch_state:uint8_t { stop, active };
+enum class tm_state:uint8_t { stop, active, pause };
+
+/**
+ * @brief in contrast with ESP-IDF this enum ALWAYS matches '0' with LOW_SPEED
+ * and '1' with HIGH_SPEED.
+ * In ESP-IDF this is all so weird:
+ * original esp32 '0' is a HIGH_SPEED
+ * derivatives (like esp32-c3, esp32-s2, etc) '0' is the LOW_SPEED
+ * Those guys must be smoking something? :)
+ */
+enum class realspeedmode_t:uint8_t { low, high };
+
+namespace ledc {
+
+struct timer {
+    ledc_timer_config_t cfg;
+    tm_state state = tm_state::stop;
+    realspeedmode_t getRealSpeedMode() const {
+#if SOC_LEDC_SUPPORT_HS_MODE
+        return cfg.speed_mode ? realspeedmode_t::low : realspeedmode_t::high;
+#else
+        return realspeedmode_t::low;
+#endif
+    }
+};
+
+struct ch {
+    ledc_channel_config_t cfg;
+    ch_state state = ch_state::stop;
+    //bool initialized = false;
+    bool idle_level = 0;
+    bool fade_cb = false;
+    realspeedmode_t getRealSpeedMode() const {
+#if SOC_LEDC_SUPPORT_HS_MODE
+        return cfg.speed_mode ? realspeedmode_t::low : realspeedmode_t::high;
+#else
+        return realspeedmode_t::low;
+#endif
+    }
+};
+
+/*
+class PWMTimers {
+    ledc_timer timers[LEDC_SPEED_MODE_MAX*LEDC_TIMER_MAX];
+public:
+    PWMTimers();
+
+    ledc_timer const *getTm(uint8_t t) const { return &timers[t % (LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX)]; };
+    int init(uint8_t tm);
+};
+
+class PWMChannels{
+    ledc_ch channels[LEDC_SPEED_MODE_MAX*LEDC_CHANNEL_MAX];
+
+public:
+    PWMChannels();   
+
+    int init(uint32_t ch, int pin = -1);
+
+    ledc_ch const *getCh(uint32_t ch) const { return &channels[ch % (LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX)]; };
+
+    int setPin(uint32_t ch, int pin, bool idlelvl = false, bool invert = false);
+//    bool ch_set_pin(int pin, uint32_t ch, bool hispeed);
+
+    int attachTimer(uint32_t ch, uint8_t timer);
+
+    int setDuty(uint32_t ch, uint32_t duty, uint32_t phase = 0);
+
+    void fade(uint32_t ch, bool dir);
+};
+*/
+
+} // namespace ledc
+
+
+class PWMCtl {
+
+    static EventGroupHandle_t g_fade_evt;
+    bool faderIRQ = false;     // fader interrupt installed
+
+public:
+    // this is a singleton
+    PWMCtl(PWMCtl const&) = delete;
+    void operator=(PWMCtl const&) = delete;
+
+    /**
+     * obtain a pointer to singleton instance
+     */
+    static PWMCtl *getInstance(){
+        static PWMCtl instance;
+        return &instance;
+    }
+
+
+    // Channel methods
+    int chStart(uint32_t ch, int pin = -1);
+    int chStop(uint32_t ch);
+    int chSet(uint32_t ch, int pin, bool idlelvl = false, bool invert = false);
+    int chFadeISR(uint32_t ch, bool enable);
+
+    esp_err_t chDuty(uint32_t ch, uint32_t duty);
+    esp_err_t chPhase(uint32_t ch, uint32_t phase);
+    esp_err_t chDutyPhase(uint32_t ch, uint32_t duty, uint32_t phase);
+
+    uint32_t chGetDuty(uint32_t ch) const;
+    uint32_t chGetMaxDuty(uint32_t ch) const;// { CH_SAFE(ch); return wrap_ledc_get_max_duty(channels[ch].cfg.speed_mode, channels[ch].cfg.channel); };
+    ledc::ch const *chGet(uint32_t ch) const { return &channels[ch % (LEDC_SPEED_MODE_MAX * LEDC_CHANNEL_MAX)]; };
+    int chAttachTimer(uint32_t ch, uint8_t timer);
+
+
+    // Timer methods
+    int tmStart(uint8_t tm);
+    esp_err_t tmSet(uint8_t tm, ledc_timer_bit_t bits, uint32_t hz);
+    esp_err_t tmSetFreq(uint8_t tm, uint32_t hz);
+    uint32_t tmGetFreq(uint8_t tm) const;
+
+    EventGroupHandle_t *getFaderEventGroup();
+
+private:
+    PWMCtl();   // hidden ctor
+    ~PWMCtl();  // hidden dtor
+
+    ledc::ch channels[LEDC_SPEED_MODE_MAX*LEDC_CHANNEL_MAX];
+    ledc::timer timers[LEDC_SPEED_MODE_MAX*LEDC_TIMER_MAX];
+
+    // construct channel default cfg
+    void chInit();
+    // construct timers default cfg
+    void tmInit();
+
+    // configure channel with current options
+    int chCfg(uint32_t ch);
+
+    /**
+    * "Fade ended" callback function will be called on any channed fade operation has ended
+    * it is called from an ISR, so I just send an event to the common Event Group and let it
+    * go outside an ISR
+    */
+// 
+    static bool IRAM_ATTR isr_fade(const ledc_cb_param_t *param, void *arg);
+};
+
+
