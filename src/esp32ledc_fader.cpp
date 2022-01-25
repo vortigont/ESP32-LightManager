2c2da56b3e933b008b485a7c27200c1e1f3580ed
Emil Muratov <gpm@hotplug.ru>
2022-01-25 15:05:54 +0300

    generic fader class + ledc fader
    a draft engine with ISR event callback

---
+++ src/esp32ledc_fader.cpp
@@ -0,0 +1,133 @@
+/*
+    ESP32 Light Manager library
+
+This code implements a library for ESP32-xx family chips and provides an
+API for controling Lighting applications, mostly (but not limited to) LEDs,
+LED strips, PWM drivers, RGB LED strips etc...
+
+
+Copyright (C) Emil Muratov, 2022
+GitHub: https://github.com/vortigont/ESP32-LightManager
+
+ *  This program or library is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General
+ *  Public License along with this library; if not, get one at
+ *  https://opensource.org/licenses/LGPL-3.0
+*/
+
+#include "esp32ledc_fader.hpp"
+
+#define EVT_TASK_NAME   "FADE_EVT"
+#define EVT_TASK_STACK  2048
+#define EVT_TASK_PRIO   2
+
+using namespace luma;
+
+
+// *** *** //
+// FadeEngineHW methods
+bool FadeEngineHW::fade(uint32_t duty, uint32_t duration){
+    return !ledc_set_fade_time_and_start(
+      PWMCtl::getInstance()->chGet(channel)->cfg.speed_mode,
+      PWMCtl::getInstance()->chGet(channel)->cfg.channel,
+      duty,
+      duration,
+      LEDC_FADE_NO_WAIT);
+}
+
+
+
+/*
+bool PWMCtl::fader_enable(bool state){
+  if (state){     // install LEDC ISR
+    if (ledc_fade_func_install(0))
+      faderIRQ = false;
+    else
+      faderIRQ = true;
+  } else {
+    ledc_fade_func_uninstall();
+    faderIRQ = false;
+  }
+  return faderIRQ;
+}
+*/
+
+FadeCtrl::FadeCtrl( uint32_t mask ) : events_mask(mask){
+  pwm = PWMCtl::getInstance();
+  eg_fade_evt = pwm->getFaderEventGroup();
+
+  //Create a task to handle UART event from ISR
+  if (eg_fade_evt)
+    xTaskCreate(FadeCtrl::evtTask, EVT_TASK_NAME, EVT_TASK_STACK, (void *)this, EVT_TASK_PRIO, &t_fade_evt);
+
+}
+
+FadeCtrl::~FadeCtrl(){
+  vTaskDelete(t_fade_evt);
+}
+
+void FadeCtrl::fd_events_handler(){
+  printf("Start fade events listener task\n");
+
+  EventBits_t x;
+
+  for (;;){
+    x = xEventGroupWaitBits(
+      *eg_fade_evt,
+      events_mask,    // group bits for monitored channels
+      pdTRUE,         // clear bits on return
+      pdFALSE,        // any bit set triggers event
+      (portTickType)portMAX_DELAY
+    );
+    printf("Got event notification for channels:");
+
+    //x &= events_mask;
+    int i = 0;
+    do {
+      if (x & 1){
+        printf(" %d", i);
+      }
+      x >>= 1;
+    } while (++i < LEDC_CHANNEL_MAX);
+    printf("\n");
+  }
+  vTaskDelete(NULL);
+}
+
+bool FadeCtrl::nofade(uint8_t ch, uint32_t duty){
+    printf("nofade ch:%d, duty:%d\n", ch, duty);
+    return PWMCtl::getInstance()->chDuty(ch, duty);
+};
+
+bool FadeCtrl::setFader(uint8_t ch, fade_engine_t fe){
+    ch %= LEDC_SPEED_MODE_MAX*LEDC_CHANNEL_MAX;
+
+    printf("setFader ch:%d, err:%d\n", ch, PWMCtl::getInstance()->chFadeISR(ch, true));
+    //PWMCtl::getInstance()->chFadeISR(ch, true);
+
+    if (!chf[ch].fe){
+      chf[ch].fe = new FadeEngineHW(ch);
+      return chf[ch].fe ? true : false;
+    }
+
+    return false;
+}
+
+bool FadeCtrl::fadebyTime(uint8_t ch, uint32_t duty, uint32_t duration){
+    ch %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX;
+    if (!chf[ch].fe){
+      return nofade(ch, duty);  // do a no-fade duty change if no FadeEngine installed for the channel 
+    }
+
+    return chf[ch].fe->fade(duty, duration);
+}
+
---
+++ src/esp32ledc_fader.hpp
@@ -0,0 +1,117 @@
+/*
+    ESP32 Light Manager library
+
+This code implements a library for ESP32-xx family chips and provides an
+API for controling Lighting applications, mostly (but not limited to) LEDs,
+LED strips, PWM drivers, RGB LED strips etc...
+
+
+Copyright (C) Emil Muratov, 2022
+GitHub: https://github.com/vortigont/ESP32-LightManager
+
+ *  This program or library is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General
+ *  Public License along with this library; if not, get one at
+ *  https://opensource.org/licenses/LGPL-3.0
+*/
+
+#pragma once
+#include "esp32ledc.hpp"
+#include "luma_curves.hpp"
+
+
+#define DEFAULT_FADE_TIME           1000           // ms
+
+
+enum class fade_engine_t:uint8_t { none, linear_hw };
+
+/**
+ * @brief Abstract Null-Fader implementation algoritm
+ * it does not fade anything, it just changes PWM duty as-is
+ * any specific implementation is meant for derived classes
+ */
+class FadeEngine {
+
+protected:
+    uint32_t channel;                               // PWMCtl channel to work on (not the esp32 channel)
+    uint32_t fade_duration = DEFAULT_FADE_TIME;
+
+public:
+    const fade_engine_t engine;
+    FadeEngine(uint32_t ch, fade_engine_t e = fade_engine_t::none) : channel(ch), engine(e) {};
+    virtual ~FadeEngine(){};
+
+    // Copy semantics : not (yet) implemented
+    FadeEngine(const FadeEngine&) = delete;
+    FadeEngine& operator=(const FadeEngine&) = delete;
+
+    virtual bool fade(uint32_t duty, uint32_t duration) = 0;     // pure virtual method, must be redefined in derived classes
+
+    //luma::curve luma_curve = luma::curve::linear;     // luma curve correction for relative methods (like xxPercent)
+};
+
+class FadeEngineHW : public FadeEngine {
+
+protected:
+
+public:
+    // Derrived constructor
+    FadeEngineHW(uint32_t ch) : FadeEngine(ch, fade_engine_t::linear_hw) { PWMCtl::getInstance()->chFadeISR(ch, true); };
+    virtual ~FadeEngineHW(){};
+
+    virtual bool fade(uint32_t duty, uint32_t duration) override;
+};
+
+
+struct ChannelFader {
+    bool state = 0;
+    //luma::curve l_curve = luma::curve::linear;     // luma curve correction for relative methods (like xxPercent)
+    FadeEngine *fe = nullptr;
+};
+
+// Fade controller class
+class FadeCtrl {
+
+    // channel faders
+    ChannelFader chf[LEDC_SPEED_MODE_MAX*LEDC_CHANNEL_MAX];
+
+    PWMCtl *pwm;
+    uint32_t events_mask;                        // bit mask for channel event group
+    TaskHandle_t t_fade_evt = nullptr;           // fade events handler
+    EventGroupHandle_t *eg_fade_evt = nullptr;
+
+    void fd_events_handler();
+
+    // static wrapper for event handler Task
+    static void evtTask(void* pvParams){
+        ((FadeCtrl*)pvParams)->fd_events_handler();
+    }
+
+    bool nofade(uint8_t ch, uint32_t duty);
+
+    //bool fadebyTime(uint8_t ch, uint32_t duty, uint32_t duration);
+
+public:
+    FadeCtrl( uint32_t mask = CH_EVENTS_BIT_MASK );
+    ~FadeCtrl();
+
+
+    bool setFader(uint8_t ch, fade_engine_t fe);
+    bool fadebyTime(uint8_t ch, uint32_t duty, uint32_t duration);
+
+    //void setCurve(uint8_t ch, luma::curve curve){ ch %= LEDC_SPEED_MODE_MAX * LEDC_TIMER_MAX; chf[ch].l_curve = curve; }
+
+    //inline virtual uint32_t setFadeDuration(uint32_t duration){ fade_duration = duration; return fade_duration; }
+    //inline virtual uint32_t getFadeDuration() const { return fade_duration; }
+
+};
+
